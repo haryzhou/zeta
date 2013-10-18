a5c448363e16e6cd882b929712ae279f3c7c20b4
haryzhou <zcman2005@gmail.com>
2013-10-18 12:15:03 +0800

    add Zeta::Timeout Zeta::Mailer::Simple

---
+++ lib/Zeta/Timeout.pm
@@ -0,0 +1,41 @@
+package Zeta::Timeout;
+use strict;
+use warnings;
+use base qw/Exporter/;
+
+our @EXPORT = qw/ztimeout/;
+
+#
+# ztimeout(
+#    \&connect, 
+#    [$dsn, $user, $pass, $opts], 
+#    15, 
+#    $logger, 
+#    '连接数据库15秒超时'
+# );
+#
+sub ztimeout {
+    my ($func, $args, $timeout, $logger, $msg);
+
+    my $rtn;
+    eval {
+        local $SIG{ALRM} = sub { die $msg; };
+        alarm($timeout);
+        $rtn = &$func(@$args);
+        alarm(0); #所需的程序已经运行完成，取消超时处理
+    };
+    if ($@) {
+        if ($@ =~ /$msg/) {
+            $logger->debug($msg);
+        }
+        else {
+            $logger->debug($@);
+        }
+        return;
+    }
+    return $rtn;
+}
+
+
+1;
+
